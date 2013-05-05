4bb83e7c8d6c4a9e96e839bdfafcba1a88b6d082
malcjohn <cincenko@seznam.cz>
2013-05-05 22:02:36 +0200

    udpa

---
+++ script.R
@@ -0,0 +1,14 @@
+library(Quandl)
+Quandl.auth("GgnxpyUBXHsyQxqp67bY")
+# Employment numbers (thousands of people") for Lansing, Michigan
+NonFarm = Quandl("FRED/LANS626NAN")
+CivLaborForce = Quandl("FRED/LANS626LFN")
+PerCapitaIncome = Quandl("FRED/LANS626PCPI")
+
+# Now let's combine the data so that we can related data values.
+Labor = merge(NonFarm, CivLaborForce, by="Date")
+Combined = merge(Labor, PerCapitaIncome, by="Date")
+colnames(Combined) = c("Date", "NonFarm", "CivLaborForce", "PerCapitaIncome")
+
+# Let's see if we can predict income as a function of employment:
+summary(lm(PerCapitaIncome~NonFarm+CivLaborForce, data=Combined))
