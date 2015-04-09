b9bd7cc3511e56581d33c0996d7b6986f72483ea
John Malc <cincenko@seznam.cz>
2015-04-09 13:53:24 +0200

    four too

--- R Programming/weekFour.R
+++ R Programming/weekFour.R
@@ -1,12 +1,21 @@
 
+# Q1
+set.seed(1)
+rpois(5, 2)
 
 
+# Q5
 
+set.seed(10)
+x <- rep(0:1, each = 5)
+e <- rnorm(10, 0, 20)
+y <- 0.5 + 2 * x + e
 
-
-
-
-
+# Q8
+library(datasets)
+Rprof()
+fit <- lm(y ~ x1 + x2)
+Rprof(NULL)
 
 
 
