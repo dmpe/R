6c7a4d1ce068852c17763a7bd3be7508a6bd9549
John Malc <cincenko@seznam.cz>
2015-04-12 13:16:18 +0200

    git work bad with rstudio.

--- R Programming/ass3/rankall.R
+++ R Programming/ass3/rankall.R
@@ -1,1 +1,17 @@
 hospital.data <- read.csv("/srv/shiny-server/R/R Programming/ass3/hospital-data.csv")
+
+
+
+
+
+
+
+
+
+rankall <- function(outcome, num = "best") {
+  ## Read outcome data
+  ## Check that state and outcome are valid
+  ## For each state, find the hospital of the given rank
+  ## Return a data frame with the hospital names and the
+  ## (abbreviated) state name
+}
