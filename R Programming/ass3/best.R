4dffa982773f1d3a58256f915dda4e498291d9b1
John Malc <cincenko@seznam.cz>
2015-04-12 11:30:52 +0200

    finished best., ready for rank

--- R Programming/ass3/best.R
+++ R Programming/ass3/best.R
@@ -4,15 +4,17 @@
 # http://stackoverflow.com/questions/10445851/converting-a-character-to-a-numeric-value-in-r
 # http://stackoverflow.com/questions/9742512/selecting-the-min-of-a-group-with-ddply
 # http://mazamascience.com/WorkingWithData/?p=929
-best("TX", "heart attack")
-best("TX", "heart failure")
-best("MD", "pneumonia")
-best("MD", "heart attack")
-best("BB", "heart attack")
-best("NY", "hert attack")
+# best("TX", "heart attack")
+# best("TX", "heart failure")
+# best("MD", "pneumonia")
+# best("MD", "heart attack")
+# best("BB", "heart attack")
+# best("NY", "hert attack")
 
-# state <- "TX"
-# outcome <- "heart attack"
+
+# source("submitscript3.R")
+# state <- "BB"
+# outcome <- "hert attack"
 
 best <- function(state, outcome) {
   df <- read.csv("outcome-of-care-measures.csv", colClasses = "character") 
@@ -24,51 +26,43 @@ best <- function(state, outcome) {
                "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia", 
                "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")]
   df[df == "Not Available"] <- NA
-  sapply(df, class)
+  df <- na.omit(df)
+  # sapply(df, class)
   
   for (i in 3:5) {
     df[,i] <- as.numeric(df[,i])
   }  
   
-  df <- na.omit(df)
+  
+  
+  if( !outcome %in% c("pneumonia", "heart failure", "heart attack") ) {
+    stop("invalid outcome")
+  }
+  
+  if( !state %in% unique(df$State) ) {
+    stop("invalid state")
+  }
   
   if(outcome == "heart attack") {
     
     df.ha <- df[, c("State", "Hospital.Name", 
                     "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")]
     df.ha <- df.ha[df.ha$State == state, ]
-    
-    return(df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack),][,2])
+    return(df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack), ][ ,2])
     
   } else if(outcome == "heart failure") {
     
     df.ha <- df[, c("State", "Hospital.Name", 
                     "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")]
     df.ha <- df.ha[df.ha$State == state, ]
+    return(df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), ][ ,2])
     
-    return(df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),][,2])
-    
-  } else if(outcome == "pneumonia") { 
+  } else { # if(outcome == "pneumonia")
     df.ha <- df[, c("State", "Hospital.Name", 
                     "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")]
     df.ha <- df.ha[df.ha$State == state, ]
+    return(df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia), ][ ,2])
     
-    return(df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia),][,2])
-  } else {
-    stop(outcome)
-#     tryCatch(stop(outcome), finally = print("invalid outcome"))
-#     tryCatch(stop(state), finally = print("invalid state"))
-#     
-#     msg <- geterrmessage()
-#     print(msg)
   }
 }
-
-
-
-
-
-
-
-
 
--- R Programming/ass3/rankhospital.R
+++ R Programming/ass3/rankhospital.R
@@ -1,12 +1,13 @@
+# source("submitscript3.R")
 
-
+# state <- "BB"
+# outcome <- "hert attack"
 
 rankhospital("TX", "heart failure", 4)
 rankhospital("MD", "heart attack", "worst")
 rankhospital("MN", "heart attack", 5000)
 
 rankhospital <- function(state, outcome, num = "best") {
-
   df <- read.csv("outcome-of-care-measures.csv", colClasses = "character") 
   df <- df[, !(colnames(df) %in% c("Provider.Number", "Address.1", "Address.2", "Address.3", 
                                    "City", "ZIP.Code", "County.Name", "Phone.Number"))]
@@ -16,13 +17,13 @@ rankhospital <- function(state, outcome, num = "best") {
                "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia", 
                "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")]
   df[df == "Not Available"] <- NA
-  sapply(df, class)
+  df <- na.omit(df)
+  # sapply(df, class)
   
   for (i in 3:5) {
     df[,i] <- as.numeric(df[,i])
   }  
   
-  df <- na.omit(df)
   
   ## Check that state and outcome are valid
   ## Return hospital name in that state with the given rank
