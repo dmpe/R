4f9f12e7ceade16f34ddff721b92c704c9afa4aa
John Malc <cincenko@seznam.cz>
2015-04-13 22:21:06 +0200

    on the way to solve

--- R Programming/ass3/rankall.R
+++ R Programming/ass3/rankall.R
@@ -1,17 +1,95 @@
-hospital.data <- read.csv("/srv/shiny-server/R/R Programming/ass3/hospital-data.csv")
 
+# https://d396qusza40orc.cloudfront.net/rprog%2Fdoc%2FProgAssignment3.pdf
+# source("submitscript3.R")
+# https://class.coursera.org/rprog-013/assignment
 
+# outcome <- "heart failure"
+# # num <- "best"
+# num <- 4
 
-
-
-
-
-
+# tail(rankall("heart failure"), 10)
+# tail(rankall("pneumonia", "worst"), 3)
+# head(rankall("heart attack", 20), 10)
+# rankall("heart attack", "best")
 
 rankall <- function(outcome, num = "best") {
-  ## Read outcome data
-  ## Check that state and outcome are valid
-  ## For each state, find the hospital of the given rank
-  ## Return a data frame with the hospital names and the
-  ## (abbreviated) state name
+  library(plyr)
+  
+  df <- read.csv("outcome-of-care-measures.csv", colClasses = "character") 
+  df <- df[, !(colnames(df) %in% c("Provider.Number", "Address.1", "Address.2", "Address.3", 
+                                   "City", "ZIP.Code", "County.Name", "Phone.Number"))]
+  
+  df <- df[, c("State", "Hospital.Name", 
+               "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", 
+               "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia", 
+               "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")]
+  df[df == "Not Available"] <- NA
+  # df <- na.omit(df)
+  
+  for (i in 3:5) {
+    df[,i] <- as.numeric(df[,i])
+  }
+  
+  if( !outcome %in% c("pneumonia", "heart failure", "heart attack") ) {
+    stop("invalid outcome")
+  }
+  
+  if( !state %in% unique(df$State) ) {
+    stop("invalid state")
+  }
+  
+  
+  if(outcome == "heart attack") {
+    
+    df.ha <- df[, c("State", "Hospital.Name", 
+                    "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")]
+    df.ha <- plyr::arrange(df.ha, df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, df.ha$Hospital.Name)
+    
+    #     df.ret <- data.frame(hospital = , state =)
+    
+    for(i in length(unique(df$State))) {
+      
+    }
+    
+    if (num == "worst") {
+      df.ha <- df.ha[which.max(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack), ][ ,2]
+    } else if (num == "best") {
+      df.ha <- df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack), ][ ,2]
+    } else {
+      df.ha <- df.ha[num, 2]
+    }
+    
+    return(df.ha)
+    
+  } else if(outcome == "heart failure") {
+    
+    df.ha <- df[, c("State", "Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")]
+    df.ha <- plyr::arrange(df.ha, df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure, df.ha$Hospital.Name)
+    
+    if(num == "worst"){
+      df.ha <- df.ha[which.max(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), ][ ,2]
+    } else if (num == "best") {
+      df.ha <- df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), ][ ,2]
+    } else {
+      df.ha <- df.ha[num, 2]
+    }
+    
+    return(df.ha)
+    
+  } else { # if(outcome == "pneumonia")
+    df.ha <- df[, c("State", "Hospital.Name", 
+                    "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")]
+    df.ha <- plyr::arrange(df.ha,df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia, df.ha$Hospital.Name)
+    
+    if(num == "worst") {
+      df.ha <- df.ha[which.max(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia), ][ ,2]
+    } else if (num == "best") {
+      df.ha <- df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia), ][ ,2]
+    } else {
+      df.ha <- df.ha[num, 2]
+    }
+    return(df.ha)
+    
+  }
+  
 }
