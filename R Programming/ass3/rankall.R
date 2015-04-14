7c9af13ba8e36b527c841a0970e6e251ad8351e9
John Malc <cincenko@seznam.cz>
2015-04-14 12:10:43 +0200

    really hard one

--- R Programming/ass3/rankall.R
+++ R Programming/ass3/rankall.R
@@ -1,16 +1,17 @@
-
-# https://d396qusza40orc.cloudfront.net/rprog%2Fdoc%2FProgAssignment3.pdf
 # source("submitscript3.R")
 # https://class.coursera.org/rprog-013/assignment
-
+# Cheated: Idea borrowed from https://github.com/danielfrg/coursera-comp-for-data-analysis/blob/master/Project%202/r-code/rankall.R 
 # outcome <- "heart failure"
 # # num <- "best"
 # num <- 4
+# i = 4
 
 # tail(rankall("heart failure"), 10)
 # tail(rankall("pneumonia", "worst"), 3)
 # head(rankall("heart attack", 20), 10)
 # rankall("heart attack", "best")
+# rankall("heart attack", 20)
+# rankall("pneumonia", "worst")
 
 rankall <- function(outcome, num = "best") {
   library(plyr)
@@ -34,62 +35,42 @@ rankall <- function(outcome, num = "best") {
     stop("invalid outcome")
   }
   
-  if( !state %in% unique(df$State) ) {
-    stop("invalid state")
-  }
-  
   
   if(outcome == "heart attack") {
-    
     df.ha <- df[, c("State", "Hospital.Name", 
                     "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")]
     df.ha <- plyr::arrange(df.ha, df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, df.ha$Hospital.Name)
-    
-    #     df.ret <- data.frame(hospital = , state =)
-    
-    for(i in length(unique(df$State))) {
-      
-    }
-    
-    if (num == "worst") {
-      df.ha <- df.ha[which.max(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack), ][ ,2]
-    } else if (num == "best") {
-      df.ha <- df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack), ][ ,2]
-    } else {
-      df.ha <- df.ha[num, 2]
-    }
-    
-    return(df.ha)
-    
+    names(df.ha)[3] = "Deaths"
   } else if(outcome == "heart failure") {
-    
     df.ha <- df[, c("State", "Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")]
     df.ha <- plyr::arrange(df.ha, df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure, df.ha$Hospital.Name)
-    
-    if(num == "worst"){
-      df.ha <- df.ha[which.max(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), ][ ,2]
-    } else if (num == "best") {
-      df.ha <- df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), ][ ,2]
-    } else {
-      df.ha <- df.ha[num, 2]
-    }
-    
-    return(df.ha)
-    
+    names(df.ha)[3] = "Deaths"
   } else { # if(outcome == "pneumonia")
     df.ha <- df[, c("State", "Hospital.Name", 
                     "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")]
     df.ha <- plyr::arrange(df.ha,df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia, df.ha$Hospital.Name)
-    
-    if(num == "worst") {
-      df.ha <- df.ha[which.max(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia), ][ ,2]
-    } else if (num == "best") {
-      df.ha <- df.ha[which.min(df.ha$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia), ][ ,2]
-    } else {
-      df.ha <- df.ha[num, 2]
-    }
-    return(df.ha)
-    
+    names(df.ha)[3] = "Deaths"
   }
+  
+  xasdasd <- split(df.ha, df.ha$State) 
+  # xasdasd <- dlply(df.ha, .(State), function(x)return(x)) ## OR with plyr
+  
+  # Thanks
+  ans = lapply(xasdasd, function(df.ha, num) {
+    if(class(num) == "character") {
+      
+      if(num == "best") {
+        return (df.ha$Hospital.Name[1])
+      } else if(num == "worst") {
+        return (df.ha$Hospital.Name[nrow(df.ha)])
+      }
+      
+    }
+    else {
+      return (df.ha$Hospital.Name[num])
+    }
+  }, num)
+  
+  return (data.frame(hospital=unlist(ans), state=names(ans)))
   
 }
--- R Programming/weekFour.R
+++ R Programming/weekFour.R
@@ -10,6 +10,7 @@ set.seed(10)
 x <- rep(0:1, each = 5)
 e <- rnorm(10, 0, 20)
 y <- 0.5 + 2 * x + e
+y
 
 # Q8
 library(datasets)
