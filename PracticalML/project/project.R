8e76d4d2f9f198f24e11812be22a61f851f9f349
dmpe <cincenko@seznam.cz>
2015-09-07 18:39:02 +0200

    prepare furhert

---
+++ PracticalML/project/project.R
@@ -0,0 +1,5 @@
+pml.testing <- read.csv("~/Documents/R/PracticalML/project/pml-testing.csv")
+sapply(pml.testing, class)
+
+pml.training <- read.csv("~/Documents/R/PracticalML/project/pml-training.csv")
+sapply(pml.training, class)
---
+++ PracticalML/quiz1.R
@@ -0,0 +1,77 @@
+#################
+#### Q1  ########
+#################
+
+library(AppliedPredictiveModeling)
+library(caret)
+data(AlzheimerDisease)
+
+adData = data.frame(diagnosis,predictors)
+trainIndex = createDataPartition(diagnosis,p=0.5,list=FALSE)
+training = adData[trainIndex,]
+testing = adData[trainIndex,]
+
+adData = data.frame(diagnosis,predictors)
+testIndex = createDataPartition(diagnosis, p = 0.50,list=FALSE)
+training = adData[-testIndex,]
+testing = adData[testIndex,]
+
+adData = data.frame(predictors)
+trainIndex = createDataPartition(diagnosis,p=0.5,list=FALSE)
+training = adData[trainIndex,]
+testing = adData[-trainIndex,]
+
+adData = data.frame(diagnosis,predictors)
+trainIndex = createDataPartition(diagnosis,p=0.5,list=FALSE)
+training = adData[trainIndex,]
+testing = adData[trainIndex,]
+
+
+
+#################
+#### Q2  ########
+#################
+library(AppliedPredictiveModeling)
+data(concrete)
+library(caret)
+set.seed(1000)
+inTrain = createDataPartition(mixtures$CompressiveStrength, p = 3/4)[[1]]
+training = mixtures[ inTrain,]
+testing = mixtures[-inTrain,]
+
+
+
+
+
+#################
+#### Q3  ########
+#################
+library(caret)
+library(AppliedPredictiveModeling)
+set.seed(3433)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+
+
+
+
+
+
+#################
+#### Q4  ########
+#################
+library(caret)
+library(AppliedPredictiveModeling)
+set.seed(3433)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+
+
+
+
