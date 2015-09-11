57e493e90250b9667f014a1e30377b044df3aa2d
dmpe <cincenko@seznam.cz>
2015-09-11 16:26:18 +0200

    add 3+4

--- PracticalML/quiz3.R
+++ PracticalML/quiz3.R
@@ -29,13 +29,24 @@ fancyRpartPlot(predictedVal)
 #################
 
 library(pgmm)
+library(tree)
 data(olive)
 olive = olive[,-1]
+newdata = as.data.frame(t(colMeans(olive)))
+
+model <- train(Area ~ ., data=olive, method="rpart")
+predict(model, newdata)
 
 
-trCl <- train(Area ~ ., method="rpart", data=trainingSub)
+trCl <- tree(olive$Area ~ ., data=olive)
+predict(trCl, newdata)
 
-print(trCl$finalModel)
+
+
+#################
+#### Q4  ########
+#################
+
 
 library(ElemStatLearn)
 data(SAheart)
@@ -44,20 +55,35 @@ train = sample(1:dim(SAheart)[1],size=dim(SAheart)[1]/2,replace=F)
 trainSA = SAheart[train,]
 testSA = SAheart[-train,]
 
+set.seed(13234)
+
+trainModel <- train(chd ~ age + alcohol + obesity + tobacco + typea + ldl, method="glm", family="binomial", data = trainSA)
+ppModel <- predict(trainModel, trainSA)
+
+print(trainModel)
+print(trainModel$finalModel)
 
 missClass = function(values,prediction){sum(((prediction > 0.5)*1) != values)/length(values)}
 
+missClass(trainSA$chd, ppModel)
+missClass(testSA$chd, predict(trainModel, testSA))
 
 
-
+#################
+#### Q5  ########
+#################
 
 
 library(ElemStatLearn)
 data(vowel.train)
 data(vowel.test) 
 
+vowel.train$y <- as.factor(vowel.train$y)
+vowel.test$y <- as.factor(vowel.test$y)
+set.seed(33833)
 
-
+trainMod <- train(y ~ . , data = vowel.train, method = "rf")
+varImp(trainMod)
 
 
 
---
+++ PracticalML/quiz4.R
@@ -0,0 +1,75 @@
+library(ElemStatLearn)
+data(vowel.train)
+data(vowel.test) 
+
+
+
+
+
+
+
+
+
+library(caret)
+library(gbm)
+set.seed(3433)
+library(AppliedPredictiveModeling)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
+
+
+
+
+
+
+
+
+
+
+
+set.seed(3523)
+library(AppliedPredictiveModeling)
+data(concrete)
+inTrain = createDataPartition(concrete$CompressiveStrength, p = 3/4)[[1]]
+training = concrete[ inTrain,]
+testing = concrete[-inTrain,]
+
+
+
+
+
+
+
+
+
+https://d396qusza40orc.cloudfront.net/predmachlearn/gaData.csv
+library(lubridate)  # For year() function below
+dat = read.csv("~/Desktop/gaData.csv")
+training = dat[year(dat$date) < 2012,]
+testing = dat[(year(dat$date)) > 2011,]
+tstrain = ts(training$visitsTumblr)
+
+
+
+
+
+
+
+
+
+
+set.seed(3523)
+library(AppliedPredictiveModeling)
+data(concrete)
+inTrain = createDataPartition(concrete$CompressiveStrength, p = 3/4)[[1]]
+training = concrete[ inTrain,]
+testing = concrete[-inTrain,]
+
+
+
+
+
+
