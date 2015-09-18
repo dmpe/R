14eac2e8f331eced1e3aef472b3d655dec614525
dmpe <cincenko@seznam.cz>
2015-09-18 14:34:36 +0200

    new exercise

--- Python-ProgEvOne/main.py
+++ Python-ProgEvOne/main.py
@@ -1,11 +1,42 @@
 __author__ = 'jm'
 
 import matplotlib as mp
-
 print "Testing my hello Wordl"
 
+#x = 15
+#x = x +52
+#print x
 
-x = 15
-x = x +52
-print x
+#name = raw_input("Enter your name")
+#print "Hello " + name
 
+## This first line is provided for you
+
+#hrs = raw_input("Enter Hours:")
+#rph = raw_input("Enter rate:")
+
+#fhrs = float(hrs)
+#frph = float(rph)
+
+#pay = fhrs * frph
+#print pay
+
+
+
+hrs = raw_input("Enter Hours:")
+h = float(hrs)
+
+rph = raw_input("Enter rph:")
+r = float(rph)
+
+
+if h <= 40:
+    pay = h * r
+    print pay    
+elif h > 40:
+    hos = h - 40
+    newR = (r * 1.5) * hos
+    print hos, newR
+    pay = (h-hos) * r + newR
+    print pay,     
+    
