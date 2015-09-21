abd1f3441e95a0b722c8a4441b2c96335957663f
dmpe <cincenko@seznam.cz>
2015-09-21 12:32:17 +0200

    pythoon exercises

--- Python-ProgEvOne/main.py
+++ Python-ProgEvOne/main.py
@@ -90,5 +90,30 @@ checking the user input unless you want to - you can assume the user types numbe
 #p = computepay(hrs,rph)
 #print "Pay",p
 
+# 5.2
+"""
+Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
+Once 'done' is entered, print out the largest and smallest of the numbers. If the user
+enters anything other than a valid number catch it with a try/except and put out an 
+appropriate message and ignore the number. Enter the numbers from the book for problem 5.1 and 
+Match the desired output as shown.
+"""
 
-
+mylist = []
+largest = None
+smallest = None
+while True:
+    input1 = raw_input("Enter a number: ")
+    
+    if input1 == "done" :
+        largest = max(mylist[0:])
+        smallest = min(mylist[0:])
+        print "Maximum is", largest, "\n", "Minimum is", smallest
+        break    
+    else:
+        try:
+            input1 = int(input1)
+            mylist.append(input1)
+        except: 
+            print "Invalid input"
+            continue
